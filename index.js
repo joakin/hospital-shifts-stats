7ca62848b6500b2988ac0e94cb474f30b1bd3745
joakin <joaquin@chimeces.com>
2015-02-24 21:00:38 +0100

    Reformat and sort the code to read top-down

--- index.js
+++ index.js
@@ -1,15 +1,55 @@
-
 var fs = require('fs')
 var _ = require('lodash')
 var glob = require('glob')
 var cheerio = require('cheerio')
-var prettyjson = require('prettyjson')
 var Table = require('cli-table');
 
-function notEmpty(x) { return !!x; }
+var stats = {}
 
-function notBoilerplate(x) {
-  return !(x.match(/2014/) || x.match(/STA LUCIA/))
+readFiles(processFiles)
+
+function readFiles(cb) {
+  glob(__dirname+'/guardias/*.html', function(err, files) {
+    if (err) console.log('problem finding files', err)
+    else cb(files)
+  })
+}
+
+function processFiles(files) {
+  var n = files.length
+  function done() {
+    if (--n === 0) statsCollected(stats)
+  }
+  files.forEach(function(f) {
+    return readFile(f, _.flow(
+      processFile.bind(null, f),
+      mergeStats,
+      done
+    ))
+  })
+}
+
+var weekDayLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+function statsCollected(stats) {
+  var table = new Table({
+    head: ['Who'].concat(weekDayLabels)
+  });
+
+  table.push.apply(table, _.map(stats, function(days, who) {
+    return [who].concat(_.range(weekDayLabels.length).map(function(i) {
+      return days[weekDayLabels[i]] || ''
+    }))
+  }).sort(function(a, b) {
+    var first = -1, later = 1
+    var keys = [7, 1, 2, 3, 4, 5, 6, 0], k
+    for (var i = 0; i < keys.length && (k=keys[i]); i++) {
+      if (a[k] > b[k]) return first
+      else if (a[k] < b[k]) return later
+    }
+    return 0;
+  }))
+
+  console.log(table.toString());
 }
 
 function readFile(path, cb) {
@@ -19,20 +59,16 @@ function readFile(path, cb) {
   })
 }
 
-function fileDate(file) {
-  var fileParts = file.match(/([0-9]{4})-([0-9]{2})\.html$/)
-  return {month: fileParts[2],
-          year:  fileParts[1]}
+function processFile(file, contents) {
+  var pieces = extractTableCells(contents)
+  var triples = cellsToTriples(pieces)
+  var entries = triplesToEntries(triples, fileDate(file))
+  var byPerson = _.groupBy(entries, 'who')
+  var byPersonWeekdayStats = entryToStats(byPerson)
+  return byPersonWeekdayStats;
 }
 
-function newEntry(date, who) {
-  return _.extend({}, date, {
-    who: who,
-    weekday: new Date(date.year, date.month-1, date.day).toString().split(' ')[0]
-  });
-}
-
-function tds(html) {
+function extractTableCells(html) {
   var $ = cheerio.load(html)
   return $('table tr').map(function(i, row) {
     return $(row).find('td').map(function(i, cell) {
@@ -61,71 +97,29 @@ function triplesToEntries(triples, date) {
   return entries
 }
 
+function newEntry(date, who) {
+  return _.extend({}, date, {
+    who: who,
+    weekday: new Date(date.year, date.month-1, date.day).toString().split(' ')[0]
+  });
+}
+
+function fileDate(file) {
+  var fileParts = file.match(/([0-9]{4})-([0-9]{2})\.html$/)
+  return {
+    month: fileParts[2],
+    year: fileParts[1]
+  }
+}
+
 function entryToStats(entries) {
   return _.mapValues(entries, function(days) {
     return _.countBy(days, 'weekday')
   })
 }
 
-function processFile(file, contents) {
-  var pieces = tds(contents)
-  var triples = cellsToTriples(pieces)
-  var entries = triplesToEntries(triples, fileDate(file))
-  var byPerson = _.groupBy(entries, 'who')
-  var byPersonWeekdayStats = entryToStats(byPerson)
-  return byPersonWeekdayStats;
-}
-
-function readFiles(cb) {
-  glob(__dirname+'/guardias/*.html', function(err, files) {
-    if (err) console.log('problem finding files', err)
-    else cb(files)
-  })
-}
-
-var stats = {}
 function mergeStats(x) {
   return _.merge(stats, x, function(a, b) {
     if (_.isArray(a)) return a.concat(b)
   });
 }
-
-function processFiles(files) {
-  var n = files.length
-  function done() {
-    if (--n === 0) statsCollected(stats)
-  }
-  files.forEach(function(f) {
-    return readFile(f, _.flow(
-      processFile.bind(null, f),
-      mergeStats,
-      done
-    ))
-  })
-}
-
-var weekDayLabels = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-
-function statsCollected(stats) {
-  var table = new Table({
-    head: ['Who'].concat(weekDayLabels)
-  });
-
-  table.push.apply(table, _.map(stats, function(days, who) {
-    return [who].concat(_.range(weekDayLabels.length).map(function(i) {
-      return days[weekDayLabels[i]] || ''
-    }))
-  }).sort(function(a, b) {
-    var first = -1, later = 1
-    var keys = [7, 1, 2, 3, 4, 5, 6, 0], k
-    for (var i = 0; i < keys.length && (k=keys[i]); i++) {
-      if (a[k] > b[k]) return first
-      else if (a[k] < b[k]) return later
-    }
-    return 0;
-  }))
-
-  console.log(table.toString());
-}
-
-readFiles(processFiles)
